b06131d43f59ea5c7fd03f05ef02add95bac95b6
geraltofrivia <pc.priyansh@gmail.com>
2019-04-06 12:57:07 +0200

    str2bool can now also just take bool inputs

--- mytorch/utils/goodies.py
+++ mytorch/utils/goodies.py
@@ -247,6 +247,8 @@ def str2bool(v)->bool:
     :param v:
     :return:
     """
+    if v.__class__ is bool:
+        return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
     elif v.lower() in ('no', 'false', 'f', 'n', '0'):
